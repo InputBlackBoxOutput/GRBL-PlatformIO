953b72ecaef6a3bcbccab5ef70f211a3769ccecf
Rutuparn Pawar <rutuparn.pawar@outlook.com>
2026-01-26 21:14:30 -0500

    Version 1.1h

--- include/protocol.h
+++ include/protocol.h
@@ -1,5 +1,23 @@
-// File: protocol.h
-// Description: Controls Grbl execution protocol and procedures
+/*
+  protocol.h - controls Grbl execution protocol and procedures
+  Part of Grbl
+
+  Copyright (c) 2011-2016 Sungeun K. Jeon for Gnea Research LLC
+  Copyright (c) 2009-2011 Simen Svale Skogsrud
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef protocol_h
 #define protocol_h
@@ -8,7 +26,7 @@
 // NOTE: Not a problem except for extreme cases, but the line buffer size can be too small
 // and g-code blocks can get truncated. Officially, the g-code standards support up to 256
 // characters. In future versions, this will be increased, when we know how much extra
-// memory space we can invest into here or we re-write the g-code parser not to have this 
+// memory space we can invest into here or we re-write the g-code parser not to have this
 // buffer.
 #ifndef LINE_BUFFER_SIZE
   #define LINE_BUFFER_SIZE 80
@@ -20,15 +38,7 @@ void protocol_main_loop();
 
 // Checks and executes a realtime command at various stop points in main program
 void protocol_execute_realtime();
-
-// Notify the stepper subsystem to start executing the g-code program in buffer.
-// void protocol_cycle_start();
-
-// Reinitializes the buffer after a feed hold for a resume.
-// void protocol_cycle_reinitialize(); 
-
-// Initiates a feed hold of the running program
-// void protocol_feed_hold();
+void protocol_exec_rt_system();
 
 // Executes the auto cycle feature, if enabled.
 void protocol_auto_cycle_start();
--- src/probe.c
+++ src/probe.c
@@ -1,5 +1,22 @@
-// File: probe.c
-// Description: Code pertaining to probing methods
+/*
+  probe.c - code pertaining to probing methods
+  Part of Grbl
+
+  Copyright (c) 2014-2016 Sungeun K. Jeon for Gnea Research LLC
+
+  Grbl is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Grbl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "grbl.h"
 
@@ -9,7 +26,7 @@ uint8_t probe_invert_mask;
 
 
 // Probe pin initialization routine.
-void probe_init() 
+void probe_init()
 {
   PROBE_DDR &= ~(PROBE_MASK); // Configure as input pins
   #ifdef DISABLE_PROBE_PIN_PULL_UP
@@ -17,13 +34,13 @@ void probe_init()
   #else
     PROBE_PORT |= PROBE_MASK;    // Enable internal pull-up resistors. Normal high operation.
   #endif
-  // probe_configure_invert_mask(false); // Initialize invert mask. Not required. Updated when in-use.
+  probe_configure_invert_mask(false); // Initialize invert mask.
 }
 
 
-// Called by probe_init() and the mc_probe() routines. Sets up the probe pin invert mask to 
-// appropriately set the pin logic according to setting for normal-high/normal-low operation 
-// and the probing cycle modes for toward-workpiece/away-from-workpiece. 
+// Called by probe_init() and the mc_probe() routines. Sets up the probe pin invert mask to
+// appropriately set the pin logic according to setting for normal-high/normal-low operation
+// and the probing cycle modes for toward-workpiece/away-from-workpiece.
 void probe_configure_invert_mask(uint8_t is_probe_away)
 {
   probe_invert_mask = 0; // Initialize as zero.
@@ -41,11 +58,9 @@ uint8_t probe_get_state() { return((PROBE_PIN & PROBE_MASK) ^ probe_invert_mask)
 // NOTE: This function must be extremely efficient as to not bog down the stepper ISR.
 void probe_state_monitor()
 {
-  if (sys_probe_state == PROBE_ACTIVE) {
-    if (probe_get_state()) {
-      sys_probe_state = PROBE_OFF;
-      memcpy(sys.probe_position, sys.position, sizeof(sys.position));
-      bit_true(sys_rt_exec_state, EXEC_MOTION_CANCEL);
-    }
+  if (probe_get_state()) {
+    sys_probe_state = PROBE_OFF;
+    memcpy(sys_probe_position, sys_position, sizeof(sys_position));
+    bit_true(sys_rt_exec_state, EXEC_MOTION_CANCEL);
   }
 }
